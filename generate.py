41e0b52b0656ebc7f00db003bfaa5611b3f28fbb
mgkos <madeline.kosten@uvm.edu>
2025-01-23 16:07:24 -0500

    ten blocks on top of each other.

--- generate.py
+++ generate.py
@@ -7,6 +7,7 @@ height = 1
 x = 0
 y = 0
 z = 0.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x+1,y+1,z+1] , size=[length,width,height])
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    z += 1
 pyrosim.End()
