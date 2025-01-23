6d8dbb79e1ecc28b712908a7c5052d97f5d12b45
mgkos <madeline.kosten@uvm.edu>
2025-01-23 16:02:44 -0500

    add two blocks stacked

--- generate.py
+++ generate.py
@@ -1,11 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 x = 0
 y = 0
-z = 1.5
+z = 0.5
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+pyrosim.Send_Cube(name="Box2", pos=[x+1,y+1,z+1] , size=[length,width,height])
 pyrosim.End()
