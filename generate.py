afe06d766c5463da05aabb0c6fa0c1776f4591d5
mgkos <madeline.kosten@uvm.edu>
2025-01-23 16:35:41 -0500

    5 by 5 grid of towers.

--- generate.py
+++ generate.py
@@ -1,16 +1,21 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = 0.5
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-    z += 1
-    length = 0.9*length
-    width = 0.9*width
-    height = 0.9*height
+x_start = -2.5
+y_start = -2.5
+z_start = 0.5
+for j in range(5):
+    for i in range(5):
+        x = x_start + i
+        y = y_start + j
+        z = z_start
+        length = 1
+        width = 1
+        height = 1
+        for k in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+            z += 1
+            length = 0.9*length
+            width = 0.9*width
+            height = 0.9*height
 pyrosim.End()
