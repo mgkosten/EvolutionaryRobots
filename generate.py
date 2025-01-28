2c315f18d9c5cfa70949f9e276b4a7fdbe083e8f
mgkos <madeline.kosten@uvm.edu>
2025-01-28 17:04:45 -0500

    Create World and Create_Robot Functions made.

--- generate.py
+++ generate.py
@@ -1,11 +1,14 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
-x = 0
-y = 0
-z = 0.5
-length = 1
-width = 1
-height = 1
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.End()
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
