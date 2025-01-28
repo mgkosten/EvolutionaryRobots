573fd7ec026295bd004d46cdd57b883417b521c7
mgkos <madeline.kosten@uvm.edu>
2025-01-28 17:12:07 -0500

    Create world and robot seperation.

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import pyrosim.pyrosim as pyrosim
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Cube(name="Box", pos=[2,2,0.5] , size=[1,1,1])
     pyrosim.End()
 
 def Create_Robot():
