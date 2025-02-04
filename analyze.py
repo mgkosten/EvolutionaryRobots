b1b08e8b0dcde784a2f5f53ea50e9ca1db37858f
mgkos <madeline.kosten@uvm.edu>
2025-02-03 20:54:20 -0500

    add graph to analyze

--- analyze.py
+++ analyze.py
@@ -1,1 +1,10 @@
 import numpy
+import matplotlib.pyplot as plt
+
+backLegSensorValues = numpy.load('data/sensorValues.npy')
+
+plt.plot(backLegSensorValues)
+
+print(backLegSensorValues)
+
+plt.show()
