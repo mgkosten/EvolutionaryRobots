2d96e4844ae1d85a395abfdf05e42280e452610a
mgkos <madeline.kosten@uvm.edu>
2025-02-03 20:34:35 -0500

    first sensor stored in vector

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -9,11 +10,15 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+
 pyrosim.Prepare_To_Simulate(robotId)
+
+backLegSensorValues = numpy.zeros(10000)
 for i in range(1000):
 	p.stepSimulation()
-	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-	print(backLegTouch)
+	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	time.sleep(1/60)
 	# print(i)
 p.disconnect()
+
+print(backLegSensorValues)
