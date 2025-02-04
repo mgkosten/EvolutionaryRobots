585ae3b38337f6829ba23b7f1095719876857650
mgkos <madeline.kosten@uvm.edu>
2025-02-03 21:14:59 -0500

    graph for front and back leg

--- analyze.py
+++ analyze.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
 frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 
-plt.plot(backLegSensorValues)
+plt.plot(backLegSensorValues, label = "BackLeg Sensor", linewidth = 4)
 
-plt.plot(frontLegSensorValues)
+plt.plot(frontLegSensorValues, label = "FrontLeg Sensor")
 
 # print(backLegSensorValues)
 
+plt.legend()
 plt.show()
