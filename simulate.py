d42cb514af8392e2411730dffce6763640c4a656
mgkos <madeline.kosten@uvm.edu>
2025-02-03 20:26:38 -0500

    first sensor readings

--- simulate.py
+++ simulate.py
@@ -13,6 +13,7 @@ pyrosim.Prepare_To_Simulate(robotId)
 for i in range(1000):
 	p.stepSimulation()
 	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	print(backLegTouch)
 	time.sleep(1/60)
-	print(i)
+	# print(i)
 p.disconnect()
