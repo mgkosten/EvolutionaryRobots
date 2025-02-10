f89fd5f9c2c7cff94cad8dd6c73e72e5cad7cbed
mgkos <madeline.kosten@uvm.edu>
2025-02-10 16:58:13 -0500

    first motor added backleg and torso.

--- simulate.py
+++ simulate.py
@@ -13,12 +13,19 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
-for i in range(1000):
+backLegSensorValues = numpy.zeros(500)
+frontLegSensorValues = numpy.zeros(500)
+for i in range(500):
 	p.stepSimulation()
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+	pyrosim.Set_Motor_For_Joint( 
+		bodyIndex = robotId, 
+		jointName = b'Torso_BackLeg', 
+		controlMode = p.POSITION_CONTROL, 
+		targetPosition = 0.0, 
+		maxForce = 500)
+
 	time.sleep(1/60)
 	# print(i)
 p.disconnect()
