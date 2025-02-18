c336d52762fedd8307bd50abb9ec4a37bd0428df
mgkos <madeline.kosten@uvm.edu>
2025-02-17 20:53:31 -0500

    destruct method of simulation added.

--- simulation.py
+++ simulation.py
@@ -39,24 +39,6 @@ class SIMULATION:
             time.sleep(1/240)  # 4 spaces indentation
             print(i)  # 4 spaces indentation
 
-    
-    # def Run(self):
-    #     for i in range(1000):
-	#         p.stepSimulation()
-	#         # backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-	#         # frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-	#         # pyrosim.Set_Motor_For_Joint( 
-	# 	    #     bodyIndex = robotId, 
-	# 	    #     jointName = b'Torso_BackLeg', 
-	# 	    #     controlMode = p.POSITION_CONTROL, 
-	# 	    #     targetPosition = targetAngles_BackLeg[i], 
-	# 	    #     maxForce = 15)
-	
-	#         # pyrosim.Set_Motor_For_Joint( 
-	# 	    #     bodyIndex = robotId, 
-	# 	    #     jointName = b'Torso_FrontLeg', 
-	# 	    #     controlMode = p.POSITION_CONTROL, 
-	# 	    #     targetPosition = targetAngles_FrontLeg[i], 
-	# 	    #     maxForce = 15)
-    #         time.sleep(1/240)
-    #         print(i)
+    def __del__(self):
+
+        p.disconnect()
