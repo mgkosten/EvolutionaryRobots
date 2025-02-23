631d46f5ac51c0d0fadae208785b9ab9f656ebc3
mgkos <madeline.kosten@uvm.edu>
2025-02-23 15:01:38 -0500

    Hidden neurons added.

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -125,3 +125,6 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(0.0)
         pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name())
+
+    def Update_Hidden_Or_Motor_Neuron(self):
+        self.Set_Value(0.0)
