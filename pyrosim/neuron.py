fd437c7c02bbad18a4d024f918a9d68b755781ee
mgkos <madeline.kosten@uvm.edu>
2025-03-03 17:01:19 -0500

    mulitple synapses

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -128,20 +128,12 @@ class NEURON:
 
     def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0.0)
-        print(self.Get_Value())
         for key in synapses:
-            # print(key[0])
-            # print(neurons[key[0]].Get_Value())
             if (key[1] == self.Get_Name()):
                 self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[key].Get_Weight(), neurons[key[0]].Get_Value())
-        print(self.Get_Value())
-        exit()
+        self.Threshold()
 
     def Allow_Presynaptic_Neuron_To_Influence_Me(self, synapse_weight, presynap_neuron):
         self.Add_To_Value(synapse_weight * presynap_neuron)
-        # print(synapse_weight)
-        # print(presynap_neuron)
-        # exit()
-        # pass
         
         
