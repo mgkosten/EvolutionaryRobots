eb37c10e2e32fba9afb943b49bd031cb0650a69d
mgkos <madeline.kosten@uvm.edu>
2025-03-03 17:19:12 -0500

    multiple synapse weights, play with speed.

--- generate.py
+++ generate.py
@@ -32,6 +32,8 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
     pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
     pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 3 , weight = 1.0 )
+    pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 4 , weight = 1.0 )
+    pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1.25 )
     pyrosim.End()
 
 Create_World()
