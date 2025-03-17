66631a7d0f87391a55e6fe0ddc2fc5a28c3f7b79
mgkos <madeline.kosten@uvm.edu>
2025-03-17 16:11:23 -0400

    running blind

--- simulation.py
+++ simulation.py
@@ -10,7 +10,7 @@ from motor import MOTOR
 class SIMULATION:
 
     def __init__(self):
-        self.physicsClient = p.connect(p.GUI)
+        self.physicsClient = p.connect(p.DIRECT)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8)
 
